a4706f8bb18e323056d9d37bb24df5b46312c963
Dario Turchi <iodariomdq@outlook.com>
2022-06-28 21:05:21 -0300

    muestra la data  por consola, recorriendo arreglo

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 
-//paso 2) utilizamos DOMContentLoaded cuando toda la pagina esta cargada y todos los elementos cargados en el Dom
-//entonces generamos un evento cuando esto pasa y ejecutamos una accion
+
+//paso3) utilizamos Fragment o Template para pintar la informacion en el Dom
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+
+//paso 2) utilizamos DOMContentLoaded cuando toda la pagina esta cargada 
 document.addEventListener('DOMContentLoaded', ()=>{
     fetchData();
 })
 
 
 // paso1) utilizamos fetch y mostramos la informacion consumida por consola
-//utilizamos async , try y catch
 const fetchData = async () => {
-
     try {
         const res = await fetch ('api.json');
-        const data = await res.json()    //transforma la informacion traida de la api y la transforma en objeto Javascript
-        console.log(data)
+        const data = await res.json();  //transforma la informacion traida de la api a Objeto
+        // console.log(data);  
+        pintarCards(data);
     } catch (error) {
         console.log(error)
     }
 }
+
+//paso 3-b) creamos una funcion que muestra la data en pantalla, o por consola
+const pintarCards = data => {
+    data.forEach(producto =>{
+        console.log(producto)
+    })
+}
+
+
 
